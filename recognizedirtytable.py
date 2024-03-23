83f2ed64488e314dd9a3186cca3280d897dd388e
Jacob Suter <115355742+suterj12@users.noreply.github.com>
2024-03-23 14:30:21 -0400

    Checks for specific items in video to recognizedirtytable.py

--- recognizedirtytable.py
+++ recognizedirtytable.py
@@ -10,7 +10,7 @@ CREDENTIALS = service_account.Credentials.from_service_account_file(
 video_client = videointelligence.VideoIntelligenceServiceClient(credentials=CREDENTIALS)
 features = [videointelligence.Feature.OBJECT_TRACKING]
 
-with io.open("data/CoveredKSA2.MOV", "rb") as file:
+with io.open("data/CoveredKSA1.MOV", "rb") as file:
     input_content = file.read()
 
 operation = video_client.annotate_video(
@@ -24,10 +24,19 @@ print("\nFinished processing.\n")
 # The first result is retrieved because a single video was processed.
 object_annotations = result.annotation_results[0].object_annotations
 
-# Get the object annotations
-object_descriptions = []
-for object_annotation in object_annotations:
-   object_descriptions.append(object_annotation.entity.description)
+#Define objects that make tables dirty.
+clutter = set(('bottle', 'packaged goods'))
 
-print("Objects in video: ", object_descriptions)
-print("\n")
+# Get the object descriptions and print them.
+object_descriptions = set()
+for object_annotation in object_annotations:
+   object_descriptions.add(object_annotation.entity.description)
+print('Object descriptions: ', object_descriptions, '\n')
+
+#Determine and report whether any of the item descriptions match known clutter.
+clean_table = clutter.isdisjoint(object_descriptions)
+if clean_table:
+    print('The table is clean!\n')
+else:
+    print('The table is dirty!\n')
+
