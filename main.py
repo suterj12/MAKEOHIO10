a1f12c4fb116d708ae8baeb4a82fbe043381c1ff
dgoorskey <dersu.goorskey@gmail.com>
2024-03-23 17:11:37 -0400

    analyze last webcam frame

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from itertools import islice
 from PIL import Image, ImageTk # for converting webcam images to show in tk
 
 webcam: Webcam = None
+lastframe = None
 
 preview: tk.Label = None
 
@@ -26,12 +27,15 @@ def update_webcam_preview():
         return
     
     frame = next(webcam)
-    print(frame.shape)
+    # print(frame.shape)
+
+    global lastframe
+    lastframe = frame
 
     # convert image data to a form suitable for tk
     pillowimg = Image.fromarray(frame)
     tkimg = ImageTk.PhotoImage(image=pillowimg)
-    print(tkimg.width(), tkimg.height())
+    # print(tkimg.width(), tkimg.height())
 
     # show the image
     global preview
@@ -39,15 +43,28 @@ def update_webcam_preview():
     preview.image = tkimg # see https://stackoverflow.com/questions/3482081/how-to-update-the-image-of-a-tkinter-label-widget
     preview.pack()
 
+def is_clean(frame) -> bool:
+    # TODO: send data to api, determine if environment is clean or dirty
+    return False
 
 def press_finish_button(event=None):
-    print('Hello, world!')
     global webcam
     if webcam is None:
         print('No webcam connected!')
         return
+    
+    # fetch image data
+    global lastframe
+    if lastframe is None:
+        print('No webcam data! Is the webcam connected?')
+        return
 
-    print('Success!')
+    if is_clean(lastframe):
+        print("Nice! Everything's clean!")
+    else:
+        print("There's still more to clean!")
+
+    # print('Success!')
 
 if __name__ == '__main__':
     root = tk.Tk()
