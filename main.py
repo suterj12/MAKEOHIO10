5255d2a9256ad3fb2d9db806e802e07c43f80b6c
dgoorskey <dersu.goorskey@gmail.com>
2024-03-23 14:15:21 -0400

    draft ui functionality

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from google.cloud import videointelligence
 from webcam import Webcam
+import tkinter as tk # base tk library
+from tkinter import ttk # newer tk widgets
 
-if __name__ == '__main__':
+def press_finish_button(event=None):
     print('Hello, world!')
 
+if __name__ == '__main__':
+    root = tk.Tk()
+    ttk.Label(root, text='KAM').pack()
+    b = ttk.Button(root, text='Finish', command=press_finish_button)
+    b.bind('<Return>', press_finish_button)
+    b.pack()
+    root.mainloop()
