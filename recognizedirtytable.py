64477d6605b197f2d459221cb4217ca96bde4549
Jacob Suter <115355742+suterj12@users.noreply.github.com>
2024-03-23 15:42:25 -0400

    Update comments

--- recognizedirtytable.py
+++ recognizedirtytable.py
@@ -10,12 +10,13 @@ CREDENTIALS = service_account.Credentials.from_service_account_file(
 video_client = videointelligence.VideoIntelligenceServiceClient(credentials=CREDENTIALS)
 features = [videointelligence.Feature.OBJECT_TRACKING]
 
-#Read in 4 videos.
+#Read in video.
 video_path = "data/KSAMerged.MOV"
 with io.open(video_path, "rb") as file:
     input_content = file.read()
 
 #Call the api to annotate the video.
+#Split the video into 4 distinct segments.
 operation = video_client.annotate_video(
     request={ "features": features, "input_content": input_content, "video_context": {"segments": [
         { "start_time_offset": "0.5s", "end_time_offset" : "1.5s" },
@@ -29,7 +30,7 @@ print("\nProcessing video for object annotations.")
 result = operation.result(timeout=500)
 print("\nFinished processing.\n")
 
-# The first result is retrieved because a single video was processed.
+# Retrieve each of the four annotated segments.
 object_annotations = []
 for annotation in result.annotation_results:
     object_annotations.append(annotation.object_annotations)
@@ -43,7 +44,7 @@ for i in range(4):
     for object_annotation in object_annotations[i]:
         object_descriptions.add(object_annotation.entity.description)
     print('Object descriptions in segment ' , i + 1, ': ', object_descriptions, '\n')
-    #Determine and report whether any of the item descriptions match known clutter.
+    #Determine and report whether any of the item descriptions in current segment match known clutter.
     clean_table = clutter.isdisjoint(object_descriptions)
     if clean_table:
         print('Table ', i + 1, ' is clean!\n')
