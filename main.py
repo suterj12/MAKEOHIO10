5ab6c510d5428e0a6f4fa84a51aeb27fa8827ffb
dgoorskey <dersu.goorskey@gmail.com>
2024-03-23 16:47:28 -0400

    show webcam preview

--- main.py
+++ main.py
@@ -4,28 +4,59 @@ import tkinter as tk # base tk library
 from tkinter import ttk # newer tk widgets
 import cv2
 from gtts import gTTS
+from itertools import islice
+from PIL import Image, ImageTk # for converting webcam images to show in tk
 
 webcam: Webcam = None
+
+preview: tk.Label = None
 
 def press_connect_webcam(event=None):
     global webcam
     webcam = Webcam(src=2)
+    if webcam is None:
+        print("Failed to connect.")
+    else:
+        print("Connected!")
+        print(f'{webcam.w}x{webcam.h}')
+
+def update_webcam_preview():
+    global webcam
+    if webcam is None:
+        return
+    
+    frame = next(webcam)
+    print(frame.shape)
+
+    # convert image data to a form suitable for tk
+    pillowimg = Image.fromarray(frame)
+    tkimg = ImageTk.PhotoImage(image=pillowimg)
+    print(tkimg.width(), tkimg.height())
+
+    # show the image
+    global preview
+    preview.config(image=tkimg)
+    preview.image = tkimg # see https://stackoverflow.com/questions/3482081/how-to-update-the-image-of-a-tkinter-label-widget
+    preview.pack()
+
 
 def press_finish_button(event=None):
     print('Hello, world!')
     global webcam
-    if webcam is not None:
-        print(webcam.w, webcam.h)
-        for frame in webcam:
-            print(frame)
-            cv2.imshow('webcam', cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        print('Success!')
+    if webcam is None:
+        print('No webcam connected!')
+        return
+
+    print('Success!')
 
 if __name__ == '__main__':
     root = tk.Tk()
     ttk.Label(root, text='KAM').pack()
+
+    # preview = tk.PhotoImage()
+    # preview.
+    preview = ttk.Label(root, image=[])
+    preview.pack()
 
     b = ttk.Button(root, text='Connect webcam', command=press_connect_webcam)
     b.bind('<Return>', press_connect_webcam)
@@ -35,4 +66,9 @@ if __name__ == '__main__':
     b.bind('<Return>', press_finish_button)
     b.pack()
 
-    root.mainloop()
+    # root.mainloop()
+
+    while True:
+        update_webcam_preview()
+        root.update_idletasks()
+        root.update()
