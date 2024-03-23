6218604cfc6975e5de18220b23162f24d0bdbee7
Jacob Suter <115355742+suterj12@users.noreply.github.com>
2024-03-23 11:37:18 -0400

    Add object tracking example code.

---
+++ objecttrackexample.py
@@ -0,0 +1,51 @@
+"""Object tracking in a local video."""
+from google.cloud import videointelligence
+
+video_client = videointelligence.VideoIntelligenceServiceClient()
+features = [videointelligence.Feature.OBJECT_TRACKING]
+
+with io.open(path, "rb") as file:
+    input_content = file.read()
+
+operation = video_client.annotate_video(
+    request={"features": features, "input_content": input_content}
+)
+print("\nProcessing video for object annotations.")
+
+result = operation.result(timeout=500)
+print("\nFinished processing.\n")
+
+# The first result is retrieved because a single video was processed.
+object_annotations = result.annotation_results[0].object_annotations
+
+# Get only the first annotation for demo purposes.
+object_annotation = object_annotations[0]
+print("Entity description: {}".format(object_annotation.entity.description))
+if object_annotation.entity.entity_id:
+    print("Entity id: {}".format(object_annotation.entity.entity_id))
+
+print(
+    "Segment: {}s to {}s".format(
+        object_annotation.segment.start_time_offset.seconds
+        + object_annotation.segment.start_time_offset.microseconds / 1e6,
+        object_annotation.segment.end_time_offset.seconds
+        + object_annotation.segment.end_time_offset.microseconds / 1e6,
+    )
+)
+
+print("Confidence: {}".format(object_annotation.confidence))
+
+# Here we print only the bounding box of the first frame in this segment
+frame = object_annotation.frames[0]
+box = frame.normalized_bounding_box
+print(
+    "Time offset of the first frame: {}s".format(
+        frame.time_offset.seconds + frame.time_offset.microseconds / 1e6
+    )
+)
+print("Bounding box position:")
+print("\tleft  : {}".format(box.left))
+print("\ttop   : {}".format(box.top))
+print("\tright : {}".format(box.right))
+print("\tbottom: {}".format(box.bottom))
+print("\n")
